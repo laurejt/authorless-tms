a70abbb30622e7127d7775ee2c0cbe4868c3fff1
Kevin Meyers <37648386+kevin-meyers@users.noreply.github.com>
2018-07-17 11:57:01 -0700

    Add ; to get_input
    Fixed to work with linux shell

--- get_input.sh
+++ get_input.sh
@@ -11,7 +11,7 @@ echo 'Generating intermediate tsv file'
 mallet info --input $IN_SEQ --print-matrix siw > $OUT_TSV
 sed -i -e '/^$/d' -e 's/ /\t/' -e 's/ /\t/' $OUT_TSV # Convert to tsv
 
-if [ $MIN_DOC_LEN -ne 0 ] then 
+if [ $MIN_DOC_LEN -ne 0 ]; then 
   n_docs=`wc -l $OUT_TSV | cut -d' ' -f1`
   perl -ni -e "print if split >= $MIN_TOKENS" $OUT_TSV
   new_n=`wc -l $OUT_TSV | cut -d' ' -f1`
